f81ea515590ceddde4a323604ee6ad44e3fabe20
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-01-11 08:48:22 -0500

    add a function to add new list items to our to do list

--- app.js
+++ app.js
@@ -2,42 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+var items = ["buy food","cook food","eat food"];
+
+app.use(bodyParser.urlencoded({extended: true}));
 // EJS
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
 
-var today = new Date();
-var currentDay = today.getDay();
-var day ="";
+  var today = new Date();
 
-switch (currentDay) {
-  case 0:
-    day = "Sunday"
-    break;
-  case 1:
-    day = "Monday"
-    break;
-  case 2:
-    day = "Tuesday"
-    break;
-  case 3:
-    day = "Wednesday"
-    break;
-  case 4:
-    day = "Thrusday"
-    break;
-  case 5:
-    day = "Friday"
-    break;
-  case 6:
-    day = "Saturday"
-    break;
-  default:
-  console.log("Error: current day is equal to: " + currentDay);
-}
+  var options ={
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-res.render("list",{kindOfDay: day});
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list",{kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
